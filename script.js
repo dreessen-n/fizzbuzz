9d2a49d143a71fa8c2a0657826b8a637051302be
dreessen-n <dreessen.edu@gmail.com>
2022-05-19 16:24:59 -0600

    Change the output to index file using js rather than print to console

--- script.js
+++ script.js
@@ -12,4 +12,30 @@ function fizzbuzz() {
     }
 }
 
-fizzbuzz();
+// fizzbuzz();
+
+// alternate version pushing print out to html
+
+function fizzbuzzHtml() {
+    for (var i = 1; i <= 100; i++) {
+        if (((i % 3) == 0 ) && ((i % 5 == 0))) {
+            const para = document.createElement('p');
+            para.innerText = 'FizzBuzz';
+            document.body.appendChild(para);
+        } else if ((i % 3) == 0) {
+            const para = document.createElement('p');
+            para.innerText = 'Fizz';
+            document.body.appendChild(para);
+        } else if ((i % 5) == 0) {
+            const para = document.createElement('p');
+            para.innerText = 'Buzz';
+            document.body.appendChild(para);
+        } else {
+            const para = document.createElement('p');
+            para.innerText = i;
+            document.body.appendChild(para);
+        }
+    }
+}
+
+fizzbuzzHtml();
